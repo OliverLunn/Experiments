225abc8e8a7c98c11f26bdfb4b76f99fa4451096
PPYOL1 <ppyol1@nottingham.ac.uk>
2023-07-07 14:50:45 +0100

    Calibration code and data added.

---
+++ calibration_data/calibration_analysis.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+#load in acc data + duty cycles
+acc_1 = np.loadtxt("acceleration1.txt", dtype=float)
+acc_2 = np.loadtxt("acceleration2.txt", dtype=float)
+acc_3 = np.loadtxt("acceleration3.txt", dtype=float)
+acc_4 = np.loadtxt("acceleration4.txt", dtype=float)
+acc_5 = np.loadtxt("acceleration5.txt", dtype=float)
+duty_cycle = np.loadtxt("duty_cycle.txt", dtype=float)
+
+#stack data columns
+acc_data = np.stack((acc_1, acc_2, acc_3, acc_4, acc_5), axis=1)
+mean_acc_data = np.mean(acc_data, axis=1) #calc mean
+std_acc_data = np.std(acc_data, axis=1) #calc st dev
+print(np.shape(std_acc_data)) 
+
+#plotting
+fig, ax = plt.figure(1), plt.axes()
+ax.errorbar(duty_cycle, mean_acc_data, yerr=std_acc_data,fmt=".", ecolor="k", capsize=2)
+ax.set_xlabel("Duty Cycle")
+ax.set_ylabel("Peak z-acceleration ($\Gamma$)")
+ax.set_title("Shaker Calibration")
+ax.grid(True)
+plt.show()
