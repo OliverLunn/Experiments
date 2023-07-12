3603bb7b81f651d91f893224570bd36e08968e68
PPYOL1 <ppyol1@nottingham.ac.uk>
2023-07-12 15:01:25 +0100

    changes to analysis.py

---
+++ phase_transitions/analysis.py
@@ -0,0 +1,92 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import moviepy.editor as mp
+from scipy.io.wavfile import read as wavread
+
+
+def order(dataframe, frame_number):
+    '''
+    Extracts the hexatic order from a pandas dataframe.
+    Calculates the magnitude of the order.
+
+    Inputs:
+    dataframe : pandas dataframe 
+    frame_number : requested frame of video
+
+    Outputs : 
+    order : array of magnitudes of heaxtic_order param
+    '''
+    frame_data = dataframe.loc[frame_number]
+    hex_order = frame_data[["hexatic_order"]]
+    order = np.abs(hex_order)
+    
+    return order
+
+def video_to_duty(video_file_path):
+    '''
+    Returns duty cycle from audio of .MP4 file
+    Extracts audio information from .MP4 file using scipy.io.wavfile and stores
+    audio information in a numpy array.
+    Extracts duty cycle information from a numpy array of audio information.
+    Input:
+    video_file_path : a file path to an .MP4 file
+    
+    Returns:
+    duty : duty cycle (as a percentage)
+
+    '''
+    clip = mp.VideoFileClip(video_file_path)
+    clip.audio.write_audiofile("audio_out.wav") #write audio file from .MP4
+    rate, data = wavread("audio_out.wav")   #read audio file
+    audio_array = data[:,0] #discard channel information
+    
+    ft = np.abs(np.fft.fft(audio_array, n=len(audio_array))) #fourier transform audio signal
+    freq = np.fft.fftfreq(len(audio_array), 1/rate)
+    peak = int(abs(freq[np.argmax(ft)]))    #find peak freq.
+    duty = (peak - 1000) / 15   #duty cycle conversion
+    duty = float(duty)
+    return duty
+
+
+path = "C:/Users/ppyol1/Documents/videos/expt_2/"
+
+data_filename_liquid = path+"19920001.hdf5"
+data_filename_solid = path+"19920070.hdf5"
+data_filename_int = path+"19920050.hdf5"
+
+data_frame_s = pd.read_hdf(data_filename_solid)
+data_frame_l = pd.read_hdf(data_filename_liquid)
+data_frame_i = pd.read_hdf(data_filename_int)
+
+data_frame_s.index.name='index'
+data_frame_l.index.name="index"
+data_frame_i.index.name="index"
+
+video_file_s = path+"19920070.MP4"
+video_file_l = path+"19920001.MP4"
+video_file_i = path+"19920050.MP4"
+
+duty_s = video_to_duty(video_file_s)
+duty_i = video_to_duty(video_file_i)
+duty_l = video_to_duty(video_file_l)
+
+frame_number = 20
+
+order_s = order(data_frame_s, frame_number)
+order_i = order(data_frame_i, frame_number)
+order_l = order(data_frame_l, frame_number)
+
+#count = np.count_nonzero(order > 0.75) / len(hexatic_order)
+
+fig, (ax1,ax2,ax3) = plt.subplots(3, 1, sharey=True)
+ax1.set_title("Distribution of |$\Psi_6$|")
+ax1.hist(order_l, bins=200)
+ax1.text(0,75,"Duty: "+str(duty_l)+"%")
+ax2.hist(order_i, bins=200)
+ax2.text(0,75,"Duty: "+str(duty_i)+"%")
+ax3.hist(order_s, bins=200)
+ax3.text(0,75,"Duty: "+str(duty_s)+"%")
+print(duty_l, duty_i, duty_s)
+
+plt.show()
--- phase_transitions/experiment_code.py
+++ phase_transitions/experiment_code.py
@@ -1,20 +1,24 @@
 import numpy as np
 import time
 from tqdm import tqdm
-from shaker import Shaker
+from shaker import shaker
 
+shaker_obj = shaker.Shaker()
+shaker_obj
 
-shaker = Shaker()
+duties = np.arange(690, 550, -2)
+d0 = 720
 
-duties = np.arange(560, 400, -5)
-duty_0 = 550
-
+shaker_obj.set_duty(720)
+time.sleep(5)
+shaker_obj.ramp(720,525,1,-1,True)
+'''
 for duty in tqdm(duties):
-    shaker.ramp(duty_0, duty, 0.5)
-    time.sleep(5)
-    shaker.set_duty_and_record(duty)
-    time.sleep(15)
-    shaker.set_duty_and_record(duty)
-    time.sleep(5)
-    duty_0 = duty
-    
+    shaker_obj.ramp(d0, duty, 0.5, -1)
+    time.sleep(3)
+    shaker_obj.set_duty_and_record(duty)
+    time.sleep(10)
+    shaker_obj.set_duty_and_record(duty)
+    time.sleep(3)
+    d0 = duty 
+'''
