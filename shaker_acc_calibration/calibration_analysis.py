584bad7b1190687049a4d65deae8bf1dc5d4063c
PPYOL1 <ppyol1@nottingham.ac.uk>
2023-07-26 10:53:46 +0100

    comments added

--- shaker_acc_calibration/calibration_analysis.py
+++ shaker_acc_calibration/calibration_analysis.py
@@ -11,20 +11,25 @@ duty_cycle = np.loadtxt("shaker_acc_calibration/duty_cycle_data.txt", dtype=floa
 
 #stack data columns
 acc_data = np.stack((acc_1, acc_2, acc_3, acc_4, acc_5), axis=1)
-mean_acc_data = np.mean(acc_data, axis=1) #calc mean
-std_acc_data = np.std(acc_data, axis=1) #calc st dev
+mean_acc_data = np.mean(acc_data, axis=1)   #calc mean
+std_acc_data = np.std(acc_data, axis=1)     #calc st dev
 
-j_acc = np.array([0.15, 0.3, 0.55, 1.3, 2.3, 3.5, 4.2, 4.8])
+j_acc = np.array([0.15, 0.3, 0.55, 1.3, 2.3, 3.5, 4.2, 4.8])    #data taken from James thesis
 j_duty = np.array([20,30,40,50,60,70,80,90])
-duty_cycle = duty_cycle / 10
+
+duty_cycle = duty_cycle / 10    #converting to percentage
+
 #plotting
 fig, ax = plt.figure(1), plt.axes()
-ax.errorbar(duty_cycle, mean_acc_data, yerr=std_acc_data,fmt=".", ecolor="b", capsize=2, label="Measured data")
-ax.plot(j_duty, j_acc, linestyle="-", marker=".", color="r", label="James's Thesis data")
-ax.plot(j_duty, np.sqrt(2)*j_acc, linestyle="-", marker=".", color="k", label="James's Thesis data x sqrt(2)")
+ax.errorbar(duty_cycle, mean_acc_data, yerr=std_acc_data,fmt=".", ecolor="b", capsize=5, label="Measured data") #errorbar plot.
+
+#ax.plot(j_duty, j_acc, linestyle="-", marker=".", color="r", label="James's Thesis data")      #plot of James's data
+#ax.plot(j_duty, np.sqrt(2)*j_acc, linestyle="-", marker=".", color="k", label="James's Thesis data x sqrt(2)")     #plot of James's data * root (2) 
+
 ax.set_xlabel("Duty Cycle")
 ax.set_ylabel("Peak z-acceleration ($\Gamma$)")
-ax.set_title("Shaker Calibration")
-ax.grid(True)
+plt.xticks()
+plt.yticks()
 ax.legend()
+ax.grid()
 plt.show()
